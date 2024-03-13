1517f058e5ad940f6206f29ed95074f13ec31031
JJB <maystar5987@gmail.com>
2024-03-13 14:04:23 +0900

    first commit

---
+++ Source/MainView.js
@@ -0,0 +1,37 @@
+
+MainView = class MainView extends AView
+{
+	constructor()
+	{
+		super()
+
+		//TODO:edit here
+
+	}
+
+	init(context, evtListener)
+	{
+		super.init(context, evtListener)
+
+		//TODO:edit here
+
+	}
+
+	onInitDone()
+	{
+		super.onInitDone()
+
+        console.log(this.board.getCell(0,0));
+
+	}
+
+	onActiveDone(isFirst)
+	{
+		super.onActiveDone(isFirst)
+
+		//TODO:edit here
+
+	}
+
+}
+
